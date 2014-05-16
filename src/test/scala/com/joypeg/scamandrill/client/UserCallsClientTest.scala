11389e907e452ea6ed495aea7a452edde107b9ce
Diego Zambelli Sessona <dzsessona@gilt.com>
2014-05-16 20:36:38 +0100

    user calls tests

--- src/test/scala/com/joypeg/scamandrill/client/UserCallsClientTest.scala
+++ src/test/scala/com/joypeg/scamandrill/client/UserCallsClientTest.scala
@@ -56,87 +56,62 @@ class UserCallsClientTest extends FlatSpec with Matchers with BeforeAndAfterAll 
     }
   }
 
-  //    "SendRaw" should "work finding some results" in {
-  //      val q = MSendRaw(raw_message = """From: sender@example.com\nTo: recipient.email@example.com\nSubject: Some Subject\n\nSome content.""")
-  //      val res = Await.result(MandrillAsyncClient.sendRaw(q), timeout)
-  //      println("res1: " + res)
-  //    }
+  "Sender" should "work getting a valid List[MSenderDataResponse] (async client)" in {
+    val res = Await.result(MandrillAsyncClient.senders(MKey()), DefaultConfig.defaultTimeout)
+    res.head.getClass shouldBe classOf[MSenderDataResponse]
+    res.exists(_.address == "scamandrill@test.com") shouldBe true
+  }
+  it should "work getting a valid List[MSenderDataResponse] (blocking client)" in {
+    MandrillBlockingClient.senders(MKey()) match {
+      case Success(res) => res.head.getClass shouldBe classOf[MSenderDataResponse]
+      case Failure(ex) => fail(ex)
+    }
+  }
+  it should "fail if the key passed is invalid, with an 'Invalid_Key' code" in {
+    MandrillBlockingClient.senders(MKey("invalidkeytest")) match {
+      case Success(res) =>
+        fail("This operation should be unsuccessful")
+      case Failure(ex: spray.httpx.UnsuccessfulResponseException) =>
+        val inernalError = MandrillError("error", -1, "Invalid_Key", "Invalid API key")
+        val expected = new MandrillResponseException(500, "Internal Server Error", inernalError)
+        MandrillResponseException(ex) shouldBe expected
+      case Failure(ex) =>
+        fail("should return an UnsuccessfulResponseException that can be parsed as MandrillResponseException")
+    }
+  }
 
-  //  "Parse" should "getting" in {
-  //    //  "MessageInfo" should "work finding some results" in {
-  //        val raw_message = MParse(raw_message = """"From: sender@example.com\nTo: recipient.email@example.com\nSubject: Some Subject\n\nSome content."""")
-  //        val res = Await.result(MandrillAsyncClient.parse(raw_message), timeout)
-  //        println("res1: " + res)
-  //    //  }
-  //  }
-
-  //  "MessageInfo" should "work finding some results" in {
-  //    val q = MMessageInfo(key = "Su9Twr4SZKU6aoWRQy4DIA", id = "66fb42789e47461d80eb140862f564d5")
-  //    val res = Await.result(MandrillAsyncClient.messageInfo(q), timeout)
-  //    println("res1: " + res)
-  //  }
-
-  //    "Content" should "work finding some results" in {
-  //      val q = MMessageInfo(key = "Su9Twr4SZKU6aoWRQy4DIA", id = "66fb42789e47461d80eb140862f564d5")
-  //      val res = Await.result(MandrillAsyncClient.content(q), timeout)
-  //      println("res1: " + res)
-  //    }
-
-
-
-  //    "SearchTimeSeries" should "work finding some results" in {
-  //      val q = MSearchTimeSeries(key = "Su9Twr4SZKU6aoWRQy4DIA", query = "email:gmail.com", date_from = "2013-01-01", date_to = "2018-01-01")
-  //      val res = Await.result(MandrillAsyncClient.searchTimeSeries(q), timeout)
-  //      println("res1: " + res)
-  //    }
-
-  //  "Search" should "work finding some results" in {
-  //    val q = MSearch(key = "Su9Twr4SZKU6aoWRQy4DIA", query = "email:gmail.com", date_from = "2013-01-01", date_to = "2018-01-01")
-  //    val res = Await.result(MandrillAsyncClient.search(q), timeout)
-  //    println("res1: " + res)
-  //  }
-
-  //  "Send" should "work getting a valid List[MSendResponse]" in {
-  //    val msg = MSendMessage(message = validMessage)
-  //    val res1 = Await.result(MandrillAsyncClient.send(msg), timeout)
-  //     println("res1: " + res1.head._id)
-  //  }
-
-  //this is is 66fb42789e47461d80eb140862f564d5
-  //  "Send real" should "work getting a valid List[MSendResponse]" in {
-  //    val msg = MSendMessage(key = "Su9Twr4SZKU6aoWRQy4DIA", message = diegoMessage)
-  //    val res1 = Await.result(MandrillAsyncClient.send(msg), timeout)
-  //    println("res1: " + res1.head._id)
-  //  }
-
-  //
-  //  it should "work when telling that the operation is async" in {
-  //    val msg = MSendMessage(message = validMessage, async = true)
-  //    val res1 = Await.result(MandrillAsyncClient.send(msg), timeout)
-  //    println("res1: " + res1)
-  //  }
-
-  //  "Info" should "work getting a valid MInfoResponse" in {
-  //    val res1 = Await.result(MandrillAsyncClient.info(MPing()), timeout)
-  //    println("res1: " + res1)
-  //  }
-  //
-  //  "Ping" should "work getting a valid MPingResponse" in {
-  //    val res1 = Await.result(MandrillAsyncClient.ping(MPing()), timeout)
-  //    res1 shouldBe MPingResponse("\"PONG!\"")
-  //  }
-  //
-  //  "Ping (version 2)" should "work getting a valid MPingResponse" in {
-  //    val res1 = Await.result(MandrillAsyncClient.ping2(MPing()), timeout)
-  //    res1 shouldBe MPingResponse("PONG!")
-  //  }
-  //
-  //  "Sender" should "work getting a valid List[MSenderDataResponse]" in {
-  //    val res1 = Await.result(MandrillAsyncClient.senders(MPing()), timeout)
-  //    println("res1: " + res1)
-  //  }
-
-
+  "Info" should "work getting a valid MInfoResponse (async client)" in {
+    val res = Await.result(MandrillAsyncClient.info(MKey()), DefaultConfig.defaultTimeout)
+    res.getClass shouldBe classOf[MInfoResponse]
+    res.username shouldBe "joypegtech@gmail.com"
+    res.created_at shouldBe "2014-05-13 12:59:13.93756"
+    res.hourly_quota shouldBe 250
+    res.public_id shouldBe "L-NrI-gTRL774TxZ_GS9oA"
+  }
+  it should "work getting a valid MInfoResponse (blocking client)" in {
+    MandrillBlockingClient.info(MKey()) match {
+      case Success(res: MInfoResponse) =>
+        res.getClass shouldBe classOf[MInfoResponse]
+        res.username shouldBe "joypegtech@gmail.com"
+        res.created_at shouldBe "2014-05-13 12:59:13.93756"
+        res.hourly_quota shouldBe 250
+        res.public_id shouldBe "L-NrI-gTRL774TxZ_GS9oA"
+      case Success(res) => fail("Excpecting a MInfoResponse object")
+      case Failure(ex) => fail(ex)
+    }
+  }
+  it should "fail if the key passed is invalid, with an 'Invalid_Key' code" in {
+    MandrillBlockingClient.info(MKey("invalidkeytest")) match {
+      case Success(res) =>
+        fail("This operation should be unsuccessful")
+      case Failure(ex: spray.httpx.UnsuccessfulResponseException) =>
+        val inernalError = MandrillError("error", -1, "Invalid_Key", "Invalid API key")
+        val expected = new MandrillResponseException(500, "Internal Server Error", inernalError)
+        MandrillResponseException(ex) shouldBe expected
+      case Failure(ex) =>
+        fail("should return an UnsuccessfulResponseException that can be parsed as MandrillResponseException")
+    }
+  }
 
   override def afterAll() {
     MandrillAsyncClient.shutdownSystem()
