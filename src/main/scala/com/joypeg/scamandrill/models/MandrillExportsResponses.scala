3bf599ac334af40a1ba9233248ae87c0c7cf550a
Diego Zambelli Sessona <dzsessona@gilt.com>
2014-05-20 17:14:57 +0100

    Export calls tested

--- src/main/scala/com/joypeg/scamandrill/models/MandrillExportsResponses.scala
+++ src/main/scala/com/joypeg/scamandrill/models/MandrillExportsResponses.scala
@@ -4,6 +4,7 @@ package com.joypeg.scamandrill.models
 case class MExportResponse(id: String,
                            created_at: String,
                            `type`: String,
-                           finished_at: String,
+                           finished_at: Option[String],
                            state: String,
-                           result_url: String) extends MandrillResponse
+                           result_url: Option[String]) extends MandrillResponse
+
---
+++ src/test/scala/com/joypeg/scamandrill/client/ExportCallsTest.scala
@@ -0,0 +1,64 @@
+package com.joypeg.scamandrill.client
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+import scala.concurrent.Await
+import com.joypeg.scamandrill.models._
+import com.joypeg.scamandrill.MandrillTestUtils._
+import com.joypeg.scamandrill.utils.SimpleLogger
+import scala.util.Failure
+import scala.util.Success
+
+class ExportCallsTest extends FlatSpec with Matchers with SimpleLogger {
+
+//  "ExportList" should "work getting a valid MExportResponse (async client)" in {
+//    val res = Await.result(MandrillAsyncClient.exportList(MKey()), DefaultConfig.defaultTimeout)
+//    res shouldBe Nil
+//  }
+//  it should "work getting a valid  MExportResponse (blocking client)" in {
+//    MandrillBlockingClient.exportList(MKey()) match {
+//      case Success(res) =>
+//        res shouldBe Nil
+//      case Failure(ex) => fail(ex)
+//    }
+//  }
+  "ExportList" should "fail if the key passed is invalid, with an 'Invalid_Key' code" in {
+    checkFailedBecauseOfInvalidKey(MandrillBlockingClient.exportList(MKey(key="invalid")))
+  }
+
+
+  "ExportInfo" should "fail if the key passed is invalid, with an 'Invalid_Key' code" in {
+    checkFailedBecauseOfInvalidKey(MandrillBlockingClient.exportInfo(MExportInfo(id = "exmaple", key="invalid")))
+  }
+
+
+//  "ExportWhitelist" should "work getting a valid MExportResponse (async client)" in {
+//    val res = Await.result(MandrillAsyncClient.exportWhitelist(MExportNotify(notify_email = "example@example.com")), DefaultConfig.defaultTimeout)
+//    res.`type` shouldBe "whitelist"
+//  }
+//  it should "work getting a valid  MExportResponse (blocking client)" in {
+//    MandrillBlockingClient.exportWhitelist(MExportNotify(notify_email = "example@example.com")) match {
+//      case Success(res) =>
+//        res.`type` shouldBe "whitelist"
+//      case Failure(ex) => fail(ex)
+//    }
+//  }
+  "ExportWhitelist" should "fail if the key passed is invalid, with an 'Invalid_Key' code" in {
+    checkFailedBecauseOfInvalidKey(MandrillBlockingClient.exportWhitelist(MExportNotify(notify_email = "example@example.com", key="invalid")))
+  }
+
+//  "ExportReject" should "work getting a valid MExportResponse (async client)" in {
+//    val res = Await.result(MandrillAsyncClient.exportReject(MExportNotify(notify_email = "examplereject@example.com")), DefaultConfig.defaultTimeout)
+//    res.`type` shouldBe "reject"
+//  }
+//  it should "work getting a valid  MExportResponse (blocking client)" in {
+//    MandrillBlockingClient.exportReject(MExportNotify(notify_email = "examplereject@example.com")) match {
+//      case Success(res) =>
+//        res.`type` shouldBe "reject"
+//      case Failure(ex) => fail(ex)
+//    }
+//  }
+  "ExportReject" should "fail if the key passed is invalid, with an 'Invalid_Key' code" in {
+    checkFailedBecauseOfInvalidKey(MandrillBlockingClient.exportReject(MExportNotify(notify_email = "examplereject@example.com", key="invalid")))
+  }
+}
