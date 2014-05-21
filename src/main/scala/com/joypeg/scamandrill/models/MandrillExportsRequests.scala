05ea83184f85b63f35faea9e13c2ae95ea43f5af
Diego Zambelli Sessona <dzsessona@gilt.com>
2014-05-21 19:31:10 +0100

    scaladocs

--- src/main/scala/com/joypeg/scamandrill/models/MandrillExportsRequests.scala
+++ src/main/scala/com/joypeg/scamandrill/models/MandrillExportsRequests.scala
@@ -1,12 +1,32 @@
 package com.joypeg.scamandrill.models
 
-
+/**
+ * The export information
+ * @param key - a valid API key
+ * @param id - an export job identifier
+ */
 case class MExportInfo(key: String = DefaultConfig.defaultKeyFromConfig,
                        id: String) extends MandrillRequest
 
+/**
+ * The export notify info
+ * @param key - a valid API key
+ * @param notify_email - an optional email address to notify when the export job has finished
+ */
 case class MExportNotify(key: String = DefaultConfig.defaultKeyFromConfig,
                          notify_email: String) extends MandrillRequest
 
+/**
+ * The export activity
+ * @param key - a valid API key
+ * @param notify_email - an optional email address to notify when the export job has finished
+ * @param date_from - start date as a UTC string in YYYY-MM-DD HH:MM:SS format
+ * @param date_to - end date as a UTC string in YYYY-MM-DD HH:MM:SS format
+ * @param tags - an array of tag names to narrow the export to; will match messages that contain ANY of the tags
+ * @param senders - an array of senders to narrow the export to
+ * @param states - an array of states to narrow the export to; messages with ANY of the states will be included
+ * @param api_keys - an array of api keys to narrow the export to; messsagse sent with ANY of the keys will be included
+ */
 case class MExportActivity(key: String = DefaultConfig.defaultKeyFromConfig,
                            notify_email: String,
                            date_from: String,
--- src/main/scala/com/joypeg/scamandrill/models/MandrillExportsResponses.scala
+++ src/main/scala/com/joypeg/scamandrill/models/MandrillExportsResponses.scala
@@ -1,6 +1,14 @@
 package com.joypeg.scamandrill.models
 
-
+/**
+ * The export response
+ * @param id - the unique identifier for this Export. Use this identifier when checking the export job's status
+ * @param created_at - the date and time that the export job was created as a UTC string in YYYY-MM-DD HH:MM:SS format
+ * @param `type` - the type of the export job
+ * @param finished_at - the date and time that the export job was finished as a UTC string in YYYY-MM-DD HH:MM:SS format, or null for jobs that have not run
+ * @param state - the export job's state
+ * @param result_url - the url for the export job's results, if the job is complete
+ */
 case class MExportResponse(id: String,
                            created_at: String,
                            `type`: String,
