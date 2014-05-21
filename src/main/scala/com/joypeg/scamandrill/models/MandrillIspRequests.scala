696a8529915d9efb6558875c5613b899fdabeb94
Diego Zambelli Sessona <dzsessona@gilt.com>
2014-05-21 19:48:01 +0100

    scaladocs done

--- src/main/scala/com/joypeg/scamandrill/models/MandrillIspRequests.scala
+++ src/main/scala/com/joypeg/scamandrill/models/MandrillIspRequests.scala
@@ -1,23 +1,57 @@
 package com.joypeg.scamandrill.models
 
+/**
+ * The Isp
+ * @param key - a valid API key
+ * @param id - th unique identifier
+ */
 case class MIspId(key: String = DefaultConfig.defaultKeyFromConfig,
                   id: String) extends MandrillRequest
 
+/**
+ * The Isp
+ * @param key - a valid API key
+ * @param ip - a dedicated IP address
+ */
 case class MIspIp(key: String = DefaultConfig.defaultKeyFromConfig,
                   ip: String) extends MandrillRequest
 
+/**
+ * The Isp pool
+ * @param key - a valid API key
+ * @param warmup - whether the ip is currently in warmup mode
+ * @param pool - the name of the new pool to add the dedicated ip to
+ */
 case class MIspPool(key: String = DefaultConfig.defaultKeyFromConfig,
                     warmup: Boolean,
                     pool: String) extends MandrillRequest
 
+/**
+ * The Isp pool info
+ * @param key - a valid API key
+ * @param pool - the name of the new pool to add the dedicated ip to
+ */
 case class MIspPoolInfo(key: String = DefaultConfig.defaultKeyFromConfig,
                         pool: String) extends MandrillRequest
 
+/**
+ * The Isp pool info
+ * @param key - a valid API key
+ * @param ip - a dedicated IP address
+ * @param pool - the name of the new pool to add the dedicated ip to
+ * @param create_pool - whether to create the pool if it does not exist; if false and the pool does not exist, an Unknown_Pool will be thrown.
+ */
 case class MIspSetPool(key: String = DefaultConfig.defaultKeyFromConfig,
                       ip: String,
                       pool: String,
                       create_pool: Boolean) extends MandrillRequest
 
+/**
+ * The dns
+ * @param key - a valid API key
+ * @param ip - a dedicated IP address
+ * @param domain - the domain name to test
+ */
 case class MIspDns(key: String = DefaultConfig.defaultKeyFromConfig,
                   ip: String,
                   domain: String) extends MandrillRequest
--- src/main/scala/com/joypeg/scamandrill/models/MandrillIspResponses.scala
+++ src/main/scala/com/joypeg/scamandrill/models/MandrillIspResponses.scala
@@ -1,13 +1,34 @@
 package com.joypeg.scamandrill.models
 
+/**
+ * Information about the ip's custom dns, if it has been configured
+ * @param enabled - a boolean indicating whether custom dns has been configured for this ip
+ * @param valid - whether the ip's custom dns is currently valid
+ * @param error - if the ip's custom dns is invalid, this will include details about the error
+ */
 case class MIspDnsResp(enabled: Boolean,
                        valid: Boolean,
                        error: String)
 
+/**
+ * Information about the ip's warmup status
+ * @param warming_up - whether the ip is currently in warmup mode
+ * @param start_at - the start time for the warmup process as a UTC string in YYYY-MM-DD HH:MM:SS format
+ * @param end_at - the end date and time for the warmup process as a UTC string in YYYY-MM-DD HH:MM:SS format
+ */
 case class MIspWarmupResp(warming_up: Boolean,
                           start_at: String,
                           end_at: String)
 
+/**
+ * Isp response
+ * @param ip - the ip address
+ * @param created_at - the date and time that the dedicated IP was created as a UTC string in YYYY-MM-DD HH:MM:SS format
+ * @param pool - the name of the pool that this dedicated IP belongs to
+ * @param domain - the domain name (reverse dns) of this dedicated IP
+ * @param custom_dns - information about the ip's custom dns, if it has been configured
+ * @param warmup - information about the ip's warmup status
+ */
 case class MIspResponse(ip: String,
                         created_at: String,
                         pool: String,
@@ -15,14 +36,39 @@ case class MIspResponse(ip: String,
                         custom_dns: MIspDnsResp,
                         warmup: MIspWarmupResp) extends MandrillResponse
 
+/**
+ * A description of the provisioning request that was created
+ * @param requested_at - the date and time that the request was created as a UTC timestamp in YYYY-MM-DD HH:MM:SS format
+ */
 case class MIspProvisionResp(requested_at: String) extends MandrillResponse
 
+/**
+ * The isp to delete
+ * @param ip - the ip address
+ * @param deleted - a boolean indicating whether the ip was successfully deleted
+ */
 case class MIspDelete(ip: String, deleted: Boolean) extends MandrillResponse
 
+/**
+ * The pool information
+ * @param name - this pool's name
+ * @param created_at - the date and time that this pool was created as a UTC timestamp in YYYY-MM-DD HH:MM:SS format
+ * @param ips - the dedicated IPs in this pool
+ */
 case class MIspInfoPool(name: String,
                          created_at: String,
                          ips: List[MIspResponse]) extends MandrillResponse
 
+/**
+ * Validation results for the domain
+ * @param valid - whether the domain name has a correctly-configured A record pointing to the ip address
+ * @param error - if valid is false, this will contain details about why the domain's A record is incorrect
+ */
 case class MIspDnsResponse(valid: Boolean, error: String) extends MandrillResponse
 
+/**
+ * Information about the status of the pool that was deleted
+ * @param pool - the name of the pool
+ * @param deleted - whether the pool was deleted
+ */
 case class MIspDeletePoolResponse(pool: String, deleted: Boolean) extends MandrillResponse
