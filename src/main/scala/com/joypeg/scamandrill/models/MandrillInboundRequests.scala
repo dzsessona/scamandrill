af9b73e815e5871054cb9681f1a6f79c4757ec7c
Diego Zambelli Sessona <dzsessona@gilt.com>
2014-05-21 19:26:39 +0100

    scaladocs

--- src/main/scala/com/joypeg/scamandrill/models/MandrillInboundRequests.scala
+++ src/main/scala/com/joypeg/scamandrill/models/MandrillInboundRequests.scala
@@ -1,22 +1,54 @@
 package com.joypeg.scamandrill.models
 
-
+/**
+ * The inbound domain
+ * @param key - a valid API key
+ * @param domain - a domain name
+ */
 case class MInboundDomain(key: String = DefaultConfig.defaultKeyFromConfig,
                           domain: String) extends MandrillRequest
 
+/**
+ * The Route
+ * @param key - a valid API key
+ * @param domain - a domain name
+ * @param pattern - the search pattern that the mailbox name should match
+ * @param url - the webhook URL where the inbound messages will be published
+ */
 case class MInboundRoute(key: String = DefaultConfig.defaultKeyFromConfig,
                           domain: String,
                           pattern: String,
                           url: String) extends MandrillRequest
 
+/**
+ * The Route
+ * @param key - a valid API key
+ * @param id - the unique identifier of an existing mailbox route
+ * @param pattern - the search pattern that the mailbox name should match
+ * @param url - the webhook URL where the inbound messages will be published
+ */
 case class MInboundUpdateRoute(key: String = DefaultConfig.defaultKeyFromConfig,
                               id: String,
                               pattern: String,
                               url: String) extends MandrillRequest
 
+/**
+ * The route
+ * @param key - a valid API key
+ * @param id - the unique identifier of an existing route
+ */
 case class MInboundDelRoute(key: String = DefaultConfig.defaultKeyFromConfig,
                             id: String) extends MandrillRequest
 
+/**
+ * Inbound raw
+ * @param key - a valid API key
+ * @param raw_message - the full MIME document of an email message
+ * @param to - optionally define the recipients to receive the message - otherwise we'll use the To, Cc, and Bcc headers provided in the document
+ * @param mail_from - the address specified in the MAIL FROM stage of the SMTP conversation. Required for the SPF check.
+ * @param helo - the identification provided by the client mta in the MTA state of the SMTP conversation. Required for the SPF check.
+ * @param client_address - the remote MTA's ip address. Optional; required for the SPF check.
+ */
 case class MInboundRaw(key: String = DefaultConfig.defaultKeyFromConfig,
                        raw_message: String,
                        to: List[String],
--- src/main/scala/com/joypeg/scamandrill/models/MandrillInboundResponses.scala
+++ src/main/scala/com/joypeg/scamandrill/models/MandrillInboundResponses.scala
@@ -1,14 +1,31 @@
 package com.joypeg.scamandrill.models
 
-
+/**
+ * Information about the inbound domain
+ * @param domain - the domain name that is accepting mail
+ * @param created_at - the date and time that the inbound domain was added as a UTC string in YYYY-MM-DD HH:MM:SS format
+ * @param valid_mx - true if this inbound domain has successfully set up an MX record to deliver mail to the Mandrill servers
+ */
 case class MInboundDomainResponse(domain: String,
                                   created_at: String,
                                   valid_mx: Boolean) extends MandrillResponse
 
+/**
+ * Information about the inbound route
+ * @param id - the unique identifier of the route
+ * @param pattern - the mailbox route pattern that the recipient matched
+ * @param url - the webhook URL that the message was posted to
+ */
 case class MInboundRouteResponse(id: String,
                                  pattern: String,
                                  url: Boolean) extends MandrillResponse
 
+/**
+ * The inbound route response
+ * @param email - the email address of the matching recipient
+ * @param pattern - the mailbox route pattern that the recipient matched
+ * @param url - the webhook URL that the message was posted to
+ */
 case class MInboundRawResponse(email: String,
                                pattern: String,
                                url: Boolean) extends MandrillResponse
