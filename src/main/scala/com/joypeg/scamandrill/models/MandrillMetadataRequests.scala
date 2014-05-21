44a23f4e885807ad0f721259a68ed3d59e1aa256
Diego Zambelli Sessona <dzsessona@gilt.com>
2014-05-21 19:34:08 +0100

    scaladocs

--- src/main/scala/com/joypeg/scamandrill/models/MandrillMetadataRequests.scala
+++ src/main/scala/com/joypeg/scamandrill/models/MandrillMetadataRequests.scala
@@ -1,8 +1,19 @@
 package com.joypeg.scamandrill.models
 
+/**
+ * The metadata
+ * @param key - a valid API key
+ * @param name - a unique identifier for the metadata field
+ * @param view_template - optional Mustache template to control how the metadata is rendered in your activity log
+ */
 case class MMeteadatapAdd(key: String = DefaultConfig.defaultKeyFromConfig,
                           name: String,
                           view_template: String) extends MandrillRequest
 
+/**
+ * The metadata
+ * @param key - a valid API key
+ * @param name - the unique identifier of the metadata field to update
+ */
 case class MMeteadatapDelete(key: String = DefaultConfig.defaultKeyFromConfig,
                             name: String) extends MandrillRequest
--- src/main/scala/com/joypeg/scamandrill/models/MandrillMetadataResponses.scala
+++ src/main/scala/com/joypeg/scamandrill/models/MandrillMetadataResponses.scala
@@ -1,6 +1,11 @@
 package com.joypeg.scamandrill.models
 
-
+/**
+ * The metadata information
+ * @param name - the unique identifier of the metadata field to update
+ * @param state - the current state of the metadata field, one of "active", "delete", or "index"
+ * @param view_template - Mustache template to control how the metadata is rendered in your activity log
+ */
 case class MIMetadataResponse(name: String,
                               state: String,
                               view_template: String) extends MandrillResponse
