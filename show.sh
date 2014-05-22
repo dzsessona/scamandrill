8651abb14a52104fb437c7c3e70acf0e9e75b883
Diego Zambelli Sessona <dzsessona@gilt.com>
2014-05-22 21:43:30 +0100

    docs

--- show.sh
+++ show.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 lessc ~/installed/bootstrap-3.1.1/less/bootstrap.less > ~/projects/github/scamandrill/jekyll/assets/css/bootstrap.css
-jekyll serve -s ~/projects/github/sbt-neo-dependencies/jekyll -d ~/projects/github/scamandrill/jekyll/_site
+jekyll serve -s ~/projects/github/scamandrill/jekyll -d ~/projects/github/scamandrill/jekyll/_site
